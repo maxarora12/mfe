d21e7172f7d5a2c5ca4c54b9bbf4d58b5d32d496
Max Arora <maxarora@Maxs-MacBook-Pro.local>
2022-11-28 22:28:04 -0800

    exp1

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp'
 
 export default () => {
     return <div>
-        <h1>Hi there!</h1>
+        <h1>Hi there! okay </h1>
         <br />
         <MarketingApp/>
         </div>
